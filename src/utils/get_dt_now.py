e0446f074f6d2e9b6e57a21c8fe44c7e8c8e3208
“joshua-hq” <joshua@happy-quality.jp>
2025-12-01 00:30:39 +0900

    timezone implementation now allows UTC by default via ZoneInfo and also JST if required, including showing the implementation in the docstirng

--- src/utils/get_dt_now.py
+++ src/utils/get_dt_now.py
@@ -1,4 +1,15 @@
-from datetime import datetime, timezone
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-def get_dt_now(tz: timezone = timezone.utc) -> datetime:
+
+def get_dt_now(tz: ZoneInfo = ZoneInfo("UTC")) -> datetime:
+    """
+    Returns the current datetime in UTC by default, but can handle user input timezones using ZoneInfo.
+
+    To get JST (Japan standard time), use dt = get_dt_now(tz=ZoneInfo("Asia/Tokyo"))
+    """
     return datetime.now(tz=tz)
+
+
+# print(get_dt_now())
+# print(get_dt_now(tz=ZoneInfo("Asia/Tokyo")))
