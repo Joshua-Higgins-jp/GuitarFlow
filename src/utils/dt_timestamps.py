6a91a5f770717a7be29e019c6dff62bf29601bfa
Joshua Higgins <higgins.joshua91@gmail.com>
2026-02-21 22:08:11 +0900

    no idea wtf was going on before. 2 simple functions that return a timezone aware datetime now from a single call.

--- src/utils/dt_timestamps.py
+++ src/utils/dt_timestamps.py
@@ -2,15 +2,8 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 
-def get_dt_now_jst(tz: ZoneInfo = ZoneInfo("Asia/Tokyo")) -> datetime:
-    """
-    Returns the current datetime in JST.
-    Returns the current datetime in UTC by default, but can handle user input timezones using ZoneInfo.
-
-    To get UTC, use dt = get_dt_now(tz=ZoneInfo("UTC")) or simply call get_dt_now_utc()
-    """
-    return datetime.now(tz=tz)
-
+def get_dt_now_jst() -> datetime:
+    return datetime.now(tz=ZoneInfo(key="Asia/Tokyo"))
 
 def get_dt_now_utc() -> datetime:
-    return get_dt_now_jst(tz=ZoneInfo("UTC"))
+    return datetime.now(tz=ZoneInfo(key="UTC"))
