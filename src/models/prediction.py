ab2c20e24c1fbb2571fd883b613dc06f6ce855d7
Joshua Higgins <higgins.joshua91@gmail.com>
2026-05-01 01:07:23 +0900

    device only cpu because gpu builds are insane

--- src/models/prediction.py
+++ src/models/prediction.py
@@ -11,11 +11,12 @@ from torchvision.models import ResNet
 
 from config.globals import ClassLabels
 
-DEVICE: torch.device = torch.device(
-    "cuda" if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available()
-    else "cpu"
-)
+# DEVICE: torch.device = torch.device(
+#     "cuda" if torch.cuda.is_available()
+#     else "mps" if torch.backends.mps.is_available()
+#     else "cpu"
+# )
+DEVICE = torch.device("cpu")
 CLASS_NAMES = ClassLabels.as_tuple()
 
 IMG_SIZE = (224, 224)
