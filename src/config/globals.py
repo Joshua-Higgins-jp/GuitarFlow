5d2ad26cc54f688dc5c4ec05c93f91a6742e167e
Joshua Higgins <higgins.joshua91@gmail.com>
2026-05-10 00:51:58 +0900

    show logs bool added, and NOT GUITAR class added to class labels

--- src/config/globals.py
+++ src/config/globals.py
@@ -2,12 +2,14 @@ from enum import StrEnum
 from typing import Tuple
 
 
+_SHOW_LOGS = False
+
+
 class ClassLabels(StrEnum):
     """Image is labeled as either electric or acoustic."""
     ACOUSTIC = "acoustic"
     ELECTRIC = "electric"
-
-    # NOT_GUITAR = "not_guitar"
+    NOT_GUITAR = "not_guitar"
 
     @classmethod
     def as_tuple(cls) -> Tuple[str, ...]:
