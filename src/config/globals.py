6fd0d0a26bda187783b95ea658be5235cf571cfe
Joshua Higgins <higgins.joshua91@gmail.com>
2026-05-14 23:29:11 +0900

    moving label enums to global since they'll be used in multiple sources

--- src/config/globals.py
+++ src/config/globals.py
@@ -14,3 +14,25 @@ class ClassLabels(StrEnum):
     @classmethod
     def as_tuple(cls) -> Tuple[str, ...]:
         return tuple(member.value for member in cls)
+
+
+class SplitLabels(StrEnum):
+    TEST = "test"
+    TRAIN = "train"
+    VAL = "val"
+
+    @classmethod
+    def as_tuple(cls) -> Tuple[str, ...]:
+        return tuple(member.value for member in cls)
+
+
+class SourceLabels(StrEnum):
+    MANUFACTURER_SITE = "manufacturer_site"
+    MYSELF = "myself"
+    PIXABAY = "pixabay"
+    REDDIT = "reddit"
+    UNSPLASH = "unsplash"
+
+    @classmethod
+    def as_tuple(cls) -> Tuple[str, ...]:
+        return tuple(member.value for member in cls)
