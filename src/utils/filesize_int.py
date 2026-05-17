af93e11b0ed606b36154b101fe3f1d586ca20322
Joshua Higgins <higgins.joshua91@gmail.com>
2026-05-18 00:04:58 +0900

    removing unused utils

--- src/utils/filesize_int.py
+++
@@ -1,9 +0,0 @@
-from os.path import getsize
-from pathlib import Path
-
-
-def get_filesize_bytes(filepath: Path) -> int:
-    """
-    Gets file size in bytes. Assumes file already exists.
-    """
-    return getsize(filename=filepath)
